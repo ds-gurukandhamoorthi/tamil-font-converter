435f9de55f0e5e9a031a018ff104daa3e9c1ff6c
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2019-01-31 11:42:15 +0100

    Showcase regexp version as Minimum Viable Product

---
+++ MVP/README.md
@@ -0,0 +1,2 @@
+Minimum Viable Product
+(using regex, and listing all values by hand)
---
+++ MVP/TamilFontConverter.jsx
@@ -0,0 +1,71 @@
+class Converter extends React.Component{
+
+    constructor(props){
+        const consonants =  'கஙசஞடணதநபமயரலவழளறனஷஜஸஹ';
+        const sampleText= 
+            'அகர முதல எழுத்தெல்லாம் ஆதி பகவன் முதற்றே உலகு'+ 
+            '\n'+
+            '\n'+
+            'அஆஇஈஉஊஎஏஐஒஓஔஃ' + '\n'+
+            'ஸ்ரீ' +
+            'க்ஷ'+
+            'க்ஷ்' +
+            'க்ஷெக்ஷேக்ஷொக்ஷோக்ஷௌ' +
+            'க்ஷக்ஷா' +
+            'க்ஷிக்ஷீக்ஷுக்ஷூ'+
+            '\n' + consonants+ '\n'+
+            consonants.split('').map(c=> c+'\u0bcd').join('') + '\n' +
+            consonants.split('').map(c=> c+'\u0bbf').join('') + '\n' +
+            consonants.split('').map(c=> c+'\u0bc0').join('') + '\n' +
+            consonants.split('').map(c=> c+'\u0bc1').join('') + '\n' +
+            consonants.split('').map(c=> c+'\u0bc2').join('') + '\n' +
+            consonants.split('').map(c=> c+'\u0bbe').join('') + '\n' +
+            consonants.split('').map(c=> c+'\u0bc6').join('') + '\n' +
+            consonants.split('').map(c=> c+'\u0bc7').join('') + '\n' +
+            consonants.split('').map(c=> c+'\u0bc8').join('') + '\n' +
+            consonants.split('').map(c=> c+'\u0bca').join('') + '\n' +
+            consonants.split('').map(c=> c+'\u0bcb').join('') + '\n' +
+            consonants.split('').map(c=> c+'\u0bcc').join('') + '\n' +
+            ''
+        ;
+        super(props);
+        this.state={
+            text: sampleText + '\n' ,
+        }
+        this.updateText = this.updateText.bind(this);
+        this.getPreview = this.getPreview.bind(this);
+
+    }
+    updateText(event){
+        this.setState(
+            {
+                text: event.target.value,
+            }
+        );
+
+    }
+    getPreview(){
+        return convertTamilFont(this.state.text);
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <div className="unicode-text">
+                    <textarea className='form-control' id="input-unicode"  rows="30" cols="60" onChange={this.updateText} value={this.state.text}>
+                    </textarea>
+                </div>
+                <div className="preview">
+                <button className='btn btn-primary' data-clipboard-target='#output' title='Copy Text in Sentamizh Font' data-placement='top'> Copy to Clipboard </button>
+                    <div id='output' dangerouslySetInnerHTML={{__html: this.getPreview()}}>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+ReactDOM.render(<Converter /> , document.querySelector('#root')); 
+    $(document).ready(function(){
+    new ClipboardJS('.btn');
+    });
